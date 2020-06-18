f09b0e3e2238a95ea61c6ce17156c79b827d07f8
Cre8ive-collins <wilsoncollins44@yahoo.com>
2020-06-18 14:58:33 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	 
+	let text = ['A FULL-STACK DEVELOPER',
+    	'A DIGITAL MARKETER','MOTIVATED', 'PASSIONATE', 'TALENTED', 'DEDICATED', 'CRE8IVE'];
+    let count = 0;
+    let index = 0;
+    let currentText = 0;
+    let letter = 0;
+    (function type(){
+    	if (count === text.length){
+    		count = 0;
+    	}
+    	currentText = text[count];
+    	letter = currentText.slice(0);
+    	    	
+    	document.querySelector('.typing').textContent = letter;
+    	if (letter.length === currentText.length){
+    		count++;
+    		index = 0;
+    	}
+    setTimeout(type , 1500)}())
+
+});
