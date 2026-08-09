3b64d59c7999afa5bbd933c075388d7c6f2f5e15
cre8ive_collins <wilsoncollins44@yahoo.com>
2026-08-09 16:50:06 +0100

    chore: use Google Drive CV link

--- data/profile.ts
+++ data/profile.ts
@@ -5,7 +5,7 @@ export const profile = {
   yearsFintech: "4+",
   email: "wilsoncollins44@yahoo.com",
   siteUrl: "https://cre8ive-collins.github.io",
-  cvUrl: "/files/collins-wilson-cv.pdf",
+  cvUrl: "https://docs.google.com/document/d/11enNYaK7bT8b0asgxmDMOGCJm4UWBDDi/edit?usp=drivesdk&ouid=102759593681520706550&rtpof=true&sd=true",
   socials: {
     linkedIn: "https://www.linkedin.com/in/cre8ivecollins/",
     github: "https://github.com/Cre8ive-collins",
